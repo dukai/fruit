fcaa34f911c48bb00ceda79bdf31bd4d7734bb86
Kyle <dukai86@gmail.com>
2016-08-14 17:11:17 +0800

    修改首页样式 添加图标字体引入等

--- src/js/app/page-main.js
+++ src/js/app/page-main.js
@@ -3,20 +3,42 @@ define(function(require, exports, module){
 
 	$('.goods .item-box').each(function(){
 		var position = $(this).position();
-		console.log(position);
-		console.log('min: ' + position.top + ' / max: ' + parseInt(position.top + $(this).height()));
+		// console.log(position);
+		// console.log('min: ' + position.top + ' / max: ' + parseInt(position.top + $(this).height()));
 		$(this).scrollspy({
 			container: $('.goods'),
 			min: position.top,
 			max: position.top + $(this).height(),
 			onEnter: function(element, position) {
-				console.log(position);
+				// console.log(position);
 				$('.main nav').find('li').removeClass('active');
 				$('.main nav').find('.' + element.dataset.target).addClass('active');
 			},
 			onLeave: function(element, position) {
 			}
 		})
-	})
+	});
+
+
+	$('.main nav li a').click(function(e){
+		e.preventDefault();
+		var targetSelector = this.hash.substr(1);
+		var pos = $('.goods header a[name="' + targetSelector + '"]').position();
+		pos.top += $('.goods').scrollTop();
+
+		$('.goods').animate({'scrollTop': pos.top});
+	});
+
+	var cart = {};
+
+	var total = 0;
+	$('.goods').on('click', '.btn-add', function(){
+		this.dataset.pid in cart ? (cart[this.dataset.pid] += 1) : (cart[this.dataset.pid] = 1);
+		total += 1;
+		$('.tab-bar .badge').html(total);
+		console.log(cart);
+
+		localStorage.cart = JSON.stringify(cart);
+	});
 
 });
