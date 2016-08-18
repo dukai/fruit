60f9db8180805043fa22fc9402195c54503e0e73
dukai <dukai86@gmail.com>
2016-08-18 18:13:45 +0800

    checkbox event

---
+++ src/js/app/page-cart.js
@@ -0,0 +1,34 @@
+define(function(require){
+    var $ = require('jquery');
+
+    $('.goods label.checkbox').click(function(){
+        $(this).toggleClass('active');
+        $(this).find('.ico').toggleClass('icon-yuanxingxuanzhongfill icon-yuanxingweixuanzhong');
+    });
+
+
+
+    $('#checkall').click(function(){
+        $(this).toggleClass('active');
+        $(this).find('.ico').toggleClass('icon-yuanxingxuanzhongfill icon-yuanxingweixuanzhong');
+
+
+        if($(this).hasClass('active')){
+            $('.goods label.checkbox').addClass('active');
+            $('.goods label.checkbox .ico').removeClass('icon-yuanxingweixuanzhong').addClass('icon-yuanxingxuanzhongfill');
+        }else{
+            $('.goods label.checkbox').removeClass('active');
+            $('.goods label.checkbox .ico').addClass('icon-yuanxingweixuanzhong').removeClass('icon-yuanxingxuanzhongfill');
+        }
+    });
+
+
+    $('#btn-submit').click(function(){
+        var list = [];
+        $('.goods label.checkbox.active').each(function(){
+            list.push(this.dataset.pid);
+        })
+        console.log(list);
+    })
+
+});
